76866b542464ad0e8fcc18175e45bd40465ff7c0
Peyton Cross <pcross2@lion.lmu.edu>
2016-02-16 14:05:00 -0800

    Set up the stream

--- scanner.js
+++ scanner.js
@@ -1,3 +1,20 @@
 fs = require(fs)
 byline = require(byline)
 {XRegExp} = require(xregexp)
+
+module.exports = function(filename, callback) {
+	var baseStream = fs.createReadStream(filename,{encoding: 'utf8'})
+	//Current error is a placeholder
+	baseStream.on('error', function(){console.log("Whoops")})
+
+	var stream = byline(baseStream, {keepEmptyLines: true})
+	var tokens = []
+	var linenumber = 0
+	stream.on('readable', function(){
+		scan(stream.read(),linenumber++,tokens)
+	})
+	stream.once('end', function() {
+		tokens.push({kind: 'EOF', lexeme:'EOF'})
+		callback(tokens)
+	})
+}
