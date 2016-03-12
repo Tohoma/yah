b110dceaba8d7bb05e6d9dbdacc7373f680346de
Trixie Roque <troque@lion.lmu.edu>
2016-03-11 19:36:40 -0800

    Modified the test slightly and added the expected tokens on the second test

--- test/scanner/scanner-test.js
+++ test/scanner/scanner-test.js
@@ -1,27 +1,32 @@
-var scan = require('../../scanner/scanner');
-var should = require('should');
-var assert = require('assert');
+var scan = require('../../scanner/scanner'),
+    should = require('should'),
+    assert = require('assert'),
+    tokenList = require('./expected/outputs.js');
 
 describe('The scanner', function () {
-	it('scans the empty file', function (done) {
-		scan('test/scanner/inputs/empty-file.yah', function (tokens) {
-			tokens.length.should.equal(1);
-			
-			done();
-		})
-	});
+    it('scans the empty file', function (done) {
+        scan('test/scanner/inputs/valid/empty-file.yah', function (tokens) {
+            var expectedTokens = tokenList.empty_tokens;
+            tokens.should.eql(expectedTokens);
+            done();
+        })
+    });
 
-	it('scans sample1.yah file', function (done) {
-		scan('test/scanner/inputs/sample1.yah', function (tokens) {
-			//TODO
-			done();
-		})
-	});
+    it('scans sample1.yah file', function (done) {
+        scan('test/scanner/inputs/valid/sample1.yah', function (tokens) {
+            var expectedTokens = tokenList.sample1_tokens;
+            tokens.should.eql(expectedTokens);
+            done();
+        })
+    });
 
-	it('scans stest1.yah file', function (done) {
-		scan('test/scanner/inputs/stest1.yah', function (tokens) {
-			//TODO
-			done();
-		})
-	})
+    it('scans the invalid sample1.yah file', function (done) {
+        scan('test/scanner/inputs/invalid/sample1.yah', function (tokens) {
+            //TODO
+            done();
+        })
+    })
 });
+
+
+
