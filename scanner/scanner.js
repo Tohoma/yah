564287215211757345969c67d76d89a346f56eb5
Trixie Roque <troque@lion.lmu.edu>
2016-03-13 03:43:15 -0700

    Added automated js formatting

--- scanner/scanner.js
+++ scanner/scanner.js
@@ -3,48 +3,64 @@ byline = require('byline')
 XRegExp = require('xregexp')
 
 module.exports = function(filename, callback) {
-	var baseStream = fs.createReadStream(filename,{encoding: 'utf8'})
-	//Current error is a placeholder
-	//baseStream.on('error', function(){console.log("I got an error")})
+    var baseStream = fs.createReadStream(filename, {
+            encoding: 'utf8'
+        })
+        //Current error is a placeholder
+        //baseStream.on('error', function(){console.log("I got an error")})
 
-	var stream = byline(baseStream, {keepEmptyLines: true})
-	var tokens = []
-	var linenumber = 0
-	stream.on('readable', function(){
-		scan(stream.read(),linenumber++,tokens)
-	})
-	stream.once('end', function() {
-		tokens.push({kind: 'EOF', lexeme:'EOF'})
-		callback(tokens)
-	})
+    var stream = byline(baseStream, {
+        keepEmptyLines: true
+    })
+    var tokens = []
+    var linenumber = 0
+    stream.on('readable', function() {
+        scan(stream.read(), linenumber++, tokens)
+    })
+    stream.once('end', function() {
+        tokens.push({
+            kind: 'EOF',
+            lexeme: 'EOF'
+        })
+        callback(tokens)
+    })
 }
 
 var scan = function(line, linenumber, tokens) {
-	var pos = 0;
-	var start = 0;
+    var pos = 0;
+    var start = 0;
 
-	var emit = function(type,word, indentation){
-		tokens.push({kind:type, lexeme:word, idlevel: indentation})
-	}
+    var emit = function(type, word, indentation) {
+        tokens.push({
+            kind: type,
+            lexeme: word,
+            idlevel: indentation
+        })
+    }
 
-	if(!line){return}
+    if (!line) {
+        return
+    }
 
-	while(true){
-		//Single line comments
-		if(line[pos]==="/"&&line[pos + 1] === "/") {break}
+    while (true) {
+        //Single line comments
+        if (line[pos] === "/" && line[pos + 1] === "/") {
+            break
+        }
 
-		//Reserved words
-		//if ()
-		if(line[pos]){ 
-		console.log(pos)
-		console.log(line[pos]);
-		pos++;
-		console.log(pos);
-		
-		} else{
-			console.log(line);
-			break}
-	}
-	
-	//console.log(line[pos]);
+        //Reserved words
+        //if ()
+        if (line[pos]) {
+            console.log(pos)
+            console.log(line[pos]);
+            pos++;
+            console.log(pos);
+
+        } else {
+            console.log(line);
+            break
+        }
+    }
+
+    //console.log(line[pos]);
 }
