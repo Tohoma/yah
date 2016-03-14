2da1af0ef45d338b7ab38574786cf777d43aaf5a
Trixie Roque <troque@lion.lmu.edu>
2016-03-13 17:32:21 -0700

    Fixed up missing closing braces

--- test/scanner/scanner-test.js
+++ test/scanner/scanner-test.js
@@ -16,11 +16,13 @@ describe('The scanner', function () {
         scan('test/scanner/inputs/valid/sample1.yah', function (tokens) {
             var expectedTokens = tokenList.sample1_tokens;
             tokens.should.eql(expectedTokens);
+        })
+    });
 
     it('scans the invalid sample1.yah file', function (done) {
         scan('test/scanner/inputs/invalid/sample1.yah', function (tokens) {
             //TODO
             done();
         })
-    })
+    });
 });
