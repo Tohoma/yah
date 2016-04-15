9c44f0b276e31de8087340505d3cdfc5f55fd98c
jb0001994 <jb0001994@aol.com>
2016-04-14 18:49:20 -0700

    generator improvements (still not tested)

--- generator/generator.js
+++ generator/generator.js
@@ -1,48 +1,172 @@
 var Generator = (function () {
 
-	generator - function (name) {
-		if ()
+	var util = require('util');
+	var HashMap, = require('hashmap').HashMap;
+	module.exports = fucntion(program) {
+		return get(program)
 	}
 
-	generator = function (program) {
-		this.indentLevel = 0;
+	indentPadding = 4;
+	indentLevel = 0;
 
-		emit("(function() {");
-		generateBlock(program.getBlock());
-		emit("}())");
+	var emit = function (line) {
+		var pad;
+		pad = indentPadding * indentLevel;
+		return console.log(Array(pad + 1).join(' ') + line);
 	}
 
-	Generator.prototype.generateBlock = function (block) {
-		this.indentLevel += 1;
+	var makeOperator = function (operator) {
+		return {
+			is: '=',
+			be: '=',
+			and: '&&',
+			or: '||',
+			eq: "==",
+			neq: '!=',
+			// gt: '>',
+			// lt: '<',
+			// geq: '>=',
+			// leq: '<=',
+			yah: 'true',
+			nah: 'false',
+			not: '!'
+		}[operator] || operator;
+	};
 
-		for each (statement in block.statements) {
-			generateStatements(statement);
+	makeVariable = (function(lastID, map) {
+		return function(v) {
+			if(!map.has(v)) {
+				map.set(v, ++lastID);
+			}
+			return '_v' + map.get(v);
+		};
+	})(0, new HashMap());
+
+	gen = function(e) {
+		return generator[e.constructor.name](e);
+	}
+
+	generator = {
+		Program: function(program) {
+			indentLevel = 0;
+			emit('function() {');
+			gen(program.block);
+			return emit('}())');
+		},
+
+		Block: function(block) {
+			var i, len, ref, statement;
+			indentLevel += 1;
+			ref = block.statements;
+			for (i = 0, len = ref.length; i < len; i++) {
+				statement = ref[i];
+				gen(statement);
+			}
+			return indentLevel--;
+		},
+
+		VariableDeclaration: function(v) {
+			var initializer = {
+				'int': '0',
+				'bool': 'false',
+			}[v.type];
+			return emit("var " + (makeVariable(v)) + " = " + initializer + ";");
+		},
+
+		AssignmentStatement: function (s) {
+			return emit((gen(s.target)) + " = " + (gen(s.source)) + ";");
+		},
+
+		ReadStatement: function(s) {
+			var i, len, ref, results, v;
+			ref = s.varrefs;
+			results = [];
+			for (i = 0, len = ref.length; i < len; i++) {
+				v = ref[i];
+
+				results.push(emit((makeVariable(v.referent)) + " = prompt();"))
+			}
+			return results;
+		},
+
+		WriteStatement: function(s) {
+			var e, i, len, ref, results;
+			ref = s.expressions;
+			results = [];
+			for (i = 0, len = ref.length; i < len; i++) {
+				e = ref[i];
+				results.push(emit("alert(" + (gen(e)) + ");"));
+			}
+			return results;
+		},
+
+		WhileStatement: function(s) {
+			emit("while (" + (gen(s.condition)) + ") {");
+			gen(s.body);
+			return emit('}');
+		},
+
+		IntegerLiteral: function(literal) {
+			return literal.toString();
+		},
+
+		BooleanLiteral: function(literal) {
+			return literal.toString();
+		},
+
+		VariableReference: function(v) {
+			return makeVariable(v.referent);
+		},
+
+		UnaryExpression: function(e) {
+			return "(" + (makeOperator(e.op.lexeme)) + " " + (gen(e.right)) + ")";
+		},
+
+		BinaryExpression: function(e) {
+			return "(" + (gen(e.left)) + " " + (makeOperator(e.op.lexeme)) + " " + (gen(e.right)) + ")";
 		}
+	};
 
-		this.indentLevel -= 1;
-	}
 
-	Generator.prototype.generateStatements (statement) {
-		emit()
-	}
 
-	Generator.prototype.generate = function (program) {
+	// generator = function (program) {
+	// 	this.indentLevel = 0;
+		
+	// }
 
-	}
+	// Generator.prototype.generateBlock = function (block) {
+	// 	this.indentLevel += 1;
 
-	Generator.prototype.synchronized = function (entity) {
+	// 	for each (statement in block.statements) {
+	// 		generateStatements(statement);
+	// 	}
 
-	}
+	// 	this.indentLevel -= 1;
+	// }
 
-	Generator.prototype.emit = function (line) {
-		pad = indentPadding * this.indentLevel;
+	// Generator.prototype.generateStatement (statement) {
+		
+	// }
 
-		if (pad == 0) {
-			console.log(line);
-		} else {
-			console.log(" ".repeat(pad) + line);
-		}
-	}
+	// Generator.prototype.generate = function (program) {
+	// 	emit("(function() {");
+	// 	generateBlock(program.getBlock());
+	// 	emit("}())");
+	// }
+
+	// Generator.prototype.synchronized = function (entity) {
+
+	// }
+
+	// Generator.prototype.emit = function (line) {
+	// 	pad = indentPadding * this.indentLevel;
+
+	// 	if (pad == 0) {
+	// 		console.log(line);
+	// 	} else {
+	// 		console.log(" ".repeat(pad) + line);
+	// 	}
+	// }
 
 
 }());
