1b05043c77823adb319d170efcf24d8f627a0b07
Trixie Roque <troque@lion.lmu.edu>
2016-04-19 15:07:35 -0700

    Fixed scanner and runner

--- yah.js
+++ yah.js
@@ -3,22 +3,20 @@
 var argv, error, generate, parse, scan;
 
 argv = require('yargs')
-    .usage('$0 [-t] [-a] [-o] [-i] [--target [x86|c|js]] filename')
+    .usage('$0 [-t] [-a] [-o] [-i] filename')
     .boolean(['t', 'a', 'o', 'i'])
     .describe('t', 'show tokens after scanning then stop')
     .describe('a', 'show abstract syntax tree after parsing then stop')
     .describe('o', 'do optimizations')
     .describe('i', 'generate and show the intermediate code then stop')
-    .describe('target', 'generate code for x86, C, or JavaScript')["default"]({
-        target: 'js'
-    })
+    .describe('target', 'generate code for JavaScript')
     .demand(1)
     .argv;
 
-scan = require('./scanner');
-parse = require('./parser');
+scan = require('./scanner/scanner');
+parse = require('./parser/parser');
 // generate = (require('./generator'))(argv.target);
-error = require('./error');
+error = require('./error/error');
 
 scan(argv._[0], function(tokens) {
     var i, len, program, t;
