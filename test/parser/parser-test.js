f7b892f645f70018dbb91737eecc0432225f7365
adrianlu95 <alu5@lion.lmu.edu>
2016-05-05 16:57:50 -0700

    added gcd as a test case, and added the yah equivalent of a print(print)

--- test/parser/parser-test.js
+++ test/parser/parser-test.js
@@ -150,7 +150,7 @@ describe('The parser', function() {
             done();
         });
     });
-
+/
     it('throws an error when given syntactically incorrect program', function(done) {
         scan('./test/parser/inputs/invalid/sample5.yah', function(tokens) {
             var priorErrorCount = error.count;
