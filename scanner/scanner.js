73a25236dda2c7eab819f847c8c9adacde240d5c
adrianlu95 <alu5@lion.lmu.edu>
2016-05-07 13:33:34 -0700

    Do not merge this yet, needs further work done to it

--- scanner/scanner.js
+++ scanner/scanner.js
@@ -133,10 +133,9 @@ var scan = function(line, linenumber, tokens, stack, idStack) {
                     stringMode = false;
                     var matchedString = line.substring(start + 1, pos);
                     emit("strlit", matchedString, idLevel, start + 1, linenumber + 1)
-
                 }
-                pos++
-                console.log(line[pos])
+                pos++;
+                // console.log(line[pos])
                 if (line[pos] === undefined) {
                     error("Yo homie, you appear to have forgotten to add closing quotes to that string there, you should fix that. Have a fantastic day.", {
                         line: linenumber + 1
